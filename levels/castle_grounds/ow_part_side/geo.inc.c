546da55fb2cce0ff4f08b8bb286f2e9cec239da9
aglab2 <aglab3@gmail.com>
2022-07-05 23:09:51 +0800

    Improve some visuals for the warp

--- levels/castle_grounds/ow_part_side/geo.inc.c
+++ levels/castle_grounds/ow_part_side/geo.inc.c
@@ -3,8 +3,10 @@
 const GeoLayout ow_part_side[] = {
 	GEO_CULLING_RADIUS(30000),
 	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, ow_part_side_Circle_mesh_layer_5),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ow_part_side_Circle_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ow_part_side_material_revert_render_settings),
+		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, ow_part_side_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
