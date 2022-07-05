4ab1dc3add8b728a06c419bfe07724fb95f6e9de
aglab2 <aglab3@gmail.com>
2022-07-05 20:47:54 +0800

    Added crash course check for warp + reonu (not finished) course entry

--- levels/castle_grounds/ow_part_side/geo.inc.c
+++ levels/castle_grounds/ow_part_side/geo.inc.c
@@ -3,7 +3,7 @@
 const GeoLayout ow_part_side[] = {
 	GEO_CULLING_RADIUS(30000),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, ow_part_side_part_mini_mesh_layer_1),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, ow_part_side_Circle_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, ow_part_side_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
